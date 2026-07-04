ee3bf52f6dd88837b071f81758f93dbeba6fbc55
tecnodeveloper <recluzedev@gmail.com>
2026-07-04 16:39:34 +0500

    update: readme file with added links and try to scrap data from draza after 2 request got blocked

--- beautifulsoap/main.py
+++ beautifulsoap/main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://quotes.toscrape.com/tag/inspirational/"
+# url = "https://www.daraz.pk/products/-i563806129-s2610082283.html?pvid=754560f2-14af-4aa1-a81d-a57fe4194764&search=jfy&scm=1007.51705.413671.0&spm=a2a0e.tm80335142.just4u.d_563806129"
 res = requests.get(url)
 
 soup = BeautifulSoup(res.text,"html.parser")
